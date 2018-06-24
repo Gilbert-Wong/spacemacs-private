5224e3b4e63b6cbb3577ecf1219997f2388faad7
Gilbert Wong <gilbertwong96@icloud.com>
2018-06-24 22:29:34 +0800

    add function to switch command and option key on mac

--- layers/gilbert-better-defaults/funcs.el
+++ layers/gilbert-better-defaults/funcs.el
@@ -9,6 +9,22 @@
 ;;
 ;;; License: GPLv3
 
+;; mac switch meta key
+(defun mac-switch-meta nil
+  "switch meta between Option and Command"
+  (interactive)
+  (if (eq mac-option-modifier nil)
+      (progn
+	      (setq mac-option-modifier 'meta)
+	      (setq mac-command-modifier 'hyper)
+	      )
+    (progn
+      (setq mac-option-modifier nil)
+      (setq mac-command-modifier 'meta)
+      )
+    )
+  )
+
 (defun indent-buffer()
   (interactive)
   (indent-region (point-min) (point-max)))
